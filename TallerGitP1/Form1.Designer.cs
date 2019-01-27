756cf8147d4eff9ead7c267dcadef2cd539aebe5
Ivan Camilo Goez Palacio <ivan.goez.palacio1@correo.icesi.edu.co>
2019-01-27 10:28:37 -0500

    Interfaz del inventario

--- TallerGitP1/Form1.Designer.cs
+++ TallerGitP1/Form1.Designer.cs
@@ -28,13 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pregunta1 = new System.Windows.Forms.Label();
+            this.pregunta2 = new System.Windows.Forms.Label();
+            this.pregunnta3 = new System.Windows.Forms.Label();
+            this.pregunta4 = new System.Windows.Forms.Label();
+            this.pregunta5 = new System.Windows.Forms.Label();
+            this.pregunta6 = new System.Windows.Forms.Label();
+            this.pregunta7 = new System.Windows.Forms.Label();
+            this.pregunta8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pregunta1
+            // 
+            this.pregunta1.AutoSize = true;
+            this.pregunta1.Location = new System.Drawing.Point(12, 33);
+            this.pregunta1.Name = "pregunta1";
+            this.pregunta1.Size = new System.Drawing.Size(166, 13);
+            this.pregunta1.TabIndex = 0;
+            this.pregunta1.Text = "1. Cantidad de comida comprada.";
+            this.pregunta1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pregunta2
+            // 
+            this.pregunta2.AutoSize = true;
+            this.pregunta2.Location = new System.Drawing.Point(15, 126);
+            this.pregunta2.Name = "pregunta2";
+            this.pregunta2.Size = new System.Drawing.Size(160, 13);
+            this.pregunta2.TabIndex = 1;
+            this.pregunta2.Text = "2. Cantidad de comida sobrante.";
+            // 
+            // pregunnta3
+            // 
+            this.pregunnta3.AutoSize = true;
+            this.pregunnta3.Location = new System.Drawing.Point(15, 224);
+            this.pregunnta3.Name = "pregunnta3";
+            this.pregunnta3.Size = new System.Drawing.Size(172, 13);
+            this.pregunnta3.TabIndex = 2;
+            this.pregunnta3.Text = "3. Cantidad de comida desechada.";
+            this.pregunnta3.Click += new System.EventHandler(this.pregunnta3_Click);
+            // 
+            // pregunta4
+            // 
+            this.pregunta4.AutoSize = true;
+            this.pregunta4.Location = new System.Drawing.Point(15, 329);
+            this.pregunta4.Name = "pregunta4";
+            this.pregunta4.Size = new System.Drawing.Size(155, 13);
+            this.pregunta4.TabIndex = 3;
+            this.pregunta4.Text = "4. Cantidad de comida residual.";
+            this.pregunta4.Click += new System.EventHandler(this.pregunta4_Click);
+            // 
+            // pregunta5
+            // 
+            this.pregunta5.AutoSize = true;
+            this.pregunta5.Location = new System.Drawing.Point(506, 33);
+            this.pregunta5.Name = "pregunta5";
+            this.pregunta5.Size = new System.Drawing.Size(167, 13);
+            this.pregunta5.TabIndex = 4;
+            this.pregunta5.Text = "5. Cantidad de comida preparada.";
+            // 
+            // pregunta6
+            // 
+            this.pregunta6.AutoSize = true;
+            this.pregunta6.Location = new System.Drawing.Point(506, 126);
+            this.pregunta6.Name = "pregunta6";
+            this.pregunta6.Size = new System.Drawing.Size(157, 13);
+            this.pregunta6.TabIndex = 5;
+            this.pregunta6.Text = "6. Cantidad de comida vendida.";
+            // 
+            // pregunta7
+            // 
+            this.pregunta7.AutoSize = true;
+            this.pregunta7.Location = new System.Drawing.Point(506, 224);
+            this.pregunta7.Name = "pregunta7";
+            this.pregunta7.Size = new System.Drawing.Size(273, 13);
+            this.pregunta7.TabIndex = 6;
+            this.pregunta7.Text = "7. Nombre del personal encargado de llenar el inventario";
+            // 
+            // pregunta8
+            // 
+            this.pregunta8.AutoSize = true;
+            this.pregunta8.Location = new System.Drawing.Point(506, 329);
+            this.pregunta8.Name = "pregunta8";
+            this.pregunta8.Size = new System.Drawing.Size(115, 13);
+            this.pregunta8.TabIndex = 7;
+            this.pregunta8.Text = "8. Fecha del inventario";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 270);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(252, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(18, 365);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(252, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(509, 69);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(236, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(509, 166);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(236, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(509, 270);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(236, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(509, 365);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(236, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(341, 403);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(113, 35);
+            this.guardar.TabIndex = 16;
+            this.guardar.Text = "Guardar Resultado";
+            this.guardar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(803, 456);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pregunta8);
+            this.Controls.Add(this.pregunta7);
+            this.Controls.Add(this.pregunta6);
+            this.Controls.Add(this.pregunta5);
+            this.Controls.Add(this.pregunta4);
+            this.Controls.Add(this.pregunnta3);
+            this.Controls.Add(this.pregunta2);
+            this.Controls.Add(this.pregunta1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Inventario";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label pregunta1;
+        private System.Windows.Forms.Label pregunta2;
+        private System.Windows.Forms.Label pregunnta3;
+        private System.Windows.Forms.Label pregunta4;
+        private System.Windows.Forms.Label pregunta5;
+        private System.Windows.Forms.Label pregunta6;
+        private System.Windows.Forms.Label pregunta7;
+        private System.Windows.Forms.Label pregunta8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- TallerGitP1/Form1.cs
+++ TallerGitP1/Form1.cs
@@ -16,5 +16,30 @@ namespace TallerGitP1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pregunta4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pregunnta3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
